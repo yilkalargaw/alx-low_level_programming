3dc1f022f44fd9a40196a6c839f4f901b0a97afd
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-17 00:03:00 +0300

    more changes to 6-sizes again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 	printf("%s\n%s\n%s\n%s\n%s\n",
-		   sprintf("Size of a char: %u byte(s)", sizeof(char))
-		   sprintf("Size of a int: %u byte(s)", sizeof(int))
-		   sprintf("Size of a long int: %u byte(s)", sizeof(long int))
-		   sprintf("Size of a long long int: %u byte(s)", sizeof(long long int))
+		   sprintf("Size of a char: %u byte(s)", sizeof(char)),
+		   sprintf("Size of a int: %u byte(s)", sizeof(int)),
+		   sprintf("Size of a long int: %u byte(s)", sizeof(long int)),
+		   sprintf("Size of a long long int: %u byte(s)", sizeof(long long int)),
 		   sprintf("Size of a float: %u byte(s)", sizeof(float)));
 	return (0);
 }
