97d389c8b3ccf7060b7527554d3613531ca3f935
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-17 00:51:29 +0300

    added spaces before bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 	printf("%s%lu%s\n%s%lu%s\n%s%lu%s\n%s%lu%s\n%s%lu%s\n",
-		   "Size of a char: ", sizeof(char), "byte(s)",
-		   "Size of a int: ", sizeof(int), "byte(s)",
-		   "Size of a long int: ", sizeof(long int), "byte(s)",
-		   "Size of a long long int: ", sizeof(long long int), "byte(s)",
-		   "Size of a float: ", sizeof(float), "byte(s)");
+		   "Size of a char: ", sizeof(char), " byte(s)",
+		   "Size of a int: ", sizeof(int), " byte(s)",
+		   "Size of a long int: ", sizeof(long int), " byte(s)",
+		   "Size of a long long int: ", sizeof(long long int), " byte(s)",
+		   "Size of a float: ", sizeof(float), " byte(s)");
 	return (0);
 }
