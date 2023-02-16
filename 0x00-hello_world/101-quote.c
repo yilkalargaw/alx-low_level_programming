a2423d071359c8c68461a3ff7af7e7d2f30dedf0
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-16 23:48:42 +0300

    first attempt at 101-quotes.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is the place we are printing a formatted string to stderr
+ *
+ * Return: returns 1
+ */
+int main(void)
+{
+
+	fprintf(stderr, "%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
