7f8bdc83a8ac4278128ab04dec3d47755b31b02a
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-16 23:17:39 +0300

    first attempt at 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - is the place we are printing a formatted string to stdout
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
