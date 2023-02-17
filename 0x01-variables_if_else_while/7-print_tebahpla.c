349da4644d2da6483562788686bc7ff5ace902c3
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-17 11:44:44 +0300

    first attempt at 7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - is the place we are printing a formatted string to stdout
+ *
+ * Return: returns 0
+ */
+int main () {
+	char ch=0;
+   for(ch = 'z' ; ch >= 'a' ; ch--)
+	  putchar(ch);
+
+   putchar(10);
+
+   return(0);
+}
