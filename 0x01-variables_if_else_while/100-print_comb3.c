c629951a6aa4108a6ba36077ec91a415f2eb2d96
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-17 19:11:09 +0300

    tried to correct indentation problems on 100-print...

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,19 @@ int main(void)
 
 	for (i = 0; i < 9; i++)
 	{
-			for (j = i + 1; j < 10; j++)
-			{
-					putchar( i + '0');
-					putchar( j + '0');
-					if (i < 8)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-			}
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar( i + '0');
+			putchar( j + '0');
+			if (i < 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
 	}
 
 	putchar(10);
+
 	return (0);
 }
