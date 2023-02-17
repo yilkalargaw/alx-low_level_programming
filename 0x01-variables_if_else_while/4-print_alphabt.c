3a7a40c7010e815115d4eb5490c96ab26e544b41
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-17 11:31:24 +0300

    first iteration for 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - is the place we are printing a formatted string to stdout
+ *
+ * Return: returns 0
+ */
+int main () {
+	char ch=0;
+	for(ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		(if ch == e || ch == q)
+			continue;
+		putchar(ch);
+	}
+
+   putchar(10);
+
+   return(0);
+}
