5ecbe69c10e05a5d3a50d85246156e08b87c7840
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-17 12:14:16 +0300

    actually corrected the space related thing for 0-pos...

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,10 +13,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
-		printf("%d is %s\n",n , "negative");
+		printf("%d is %s\n", n, "negative");
 	else if (n == 0)
 		printf("0 is zero\n");
 	else
-		printf("%d is %s\n",n , "positive");
+		printf("%d is %s\n", n, "positive");
 	return (0);
 }
