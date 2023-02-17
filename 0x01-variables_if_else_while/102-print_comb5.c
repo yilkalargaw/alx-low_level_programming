c51403e6610c8c1af99048b5c9fab909f9e45def
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-17 19:59:42 +0300

    first attempt at 102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - is the place we are printing a formatted string to stdout
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	unsigned int i;
+	unsigned int w;
+	unsigned int x;
+	unsigned int y;
+	unsigned int z;
+
+	for (i = 0; i < 10000; i++)
+	{
+		w = i / 1000;
+		x = (i % 1000) / 100;
+		y = (i % 100) / 10;
+		z = i % 10;
+
+		if (w < y || (w == y && y < z))
+		{
+			putchar('0' + w);
+			putchar('0' + x);
+			putchar(' ');
+			putchar('0' + y);
+			putchar('0' + z);
+
+			if (!(w == 8 && x == 9 && y==9 && z==9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
