c36628bfce86efa3b5ac23e74b00c0213ccfdfc3
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-17 19:14:34 +0300

    correcting more indentation related issues on 100-print..

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - is the place we are printing a formatted string to stdout
+ *
+ * Return: returns 0
+ */
 int main(void)
 {
 	unsigned int i;
@@ -9,13 +13,13 @@ int main(void)
 	{
 		for (j = i + 1; j < 10; j++)
 		{
-			putchar( i + '0');
-			putchar( j + '0');
+			putchar(i + '0');
+			putchar(j + '0');
 			if (i < 8)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
