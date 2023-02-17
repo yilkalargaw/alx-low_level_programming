9e4ef14b39af01362884c6a3392cc6bcb037d9ec
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-17 19:16:18 +0300

    first attempt at 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - is the place we are printing a formatted string to stdout
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+
+				if (i < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+
+	return (0);
+}
