36cfa7b696748581b49730166c5222d31de30559
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-21 21:39:47 +0300

    attempted 4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - short description
+ * @c: input character
+ * Return: 0 if lower
+ */
+int _islower(int c)
+{
+	if (('a' <= num && 'z' >= num) &&
+		('A' <= num && 'Z' >= num))
+		return (1);
+	else
+		return (0);
+}
