943544d778a8d20000bba5d7fb16b64b34eddd72
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-22 01:33:56 +0300

    added the decreament condtion in 11-print_to...

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,6 +10,15 @@ void print_to_98(int num)
 {
 	int i;
 
-	for (i = num ; i <= 98; i++)
-		printf("%d%s", i, (i == 98) ? "\n" : ", ");
+	if (num <= 98)
+	{
+		for (i = num ; i <= 98; i++)
+			printf("%d%s", i, (i == 98) ? "\n" : ", ");
+	}
+	else
+	{
+		for (i = num ; i >= 98; i--)
+			printf("%d%s", i, (i == 98) ? "\n" : ", ");
+	}
+
 }
