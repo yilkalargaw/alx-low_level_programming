ec1ed757b8e82f27d90739ff80c6f3443966bb2f
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-21 21:41:10 +0300

    corrected the function names for 4-main

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _islower - short description
+ * _isalpha - short description
  * @c: input character
  * Return: 0 if lower
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (('a' <= num && 'z' >= num) &&
 		('A' <= num && 'Z' >= num))
