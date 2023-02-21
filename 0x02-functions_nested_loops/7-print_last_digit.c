0469b82ceafb0c7b48a9f04fb2a21687f4c6861d
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-21 23:32:10 +0300

    now added the offset in the putchar statement

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,6 @@ int print_last_digit(int num)
 {
 	unsigned int lastdigit;
 	(num >= 0) ? (lastdigit = num % 10) : (lastdigit = (-1 * num) % 10);
-	_putchar(lastdigit);
+	_putchar('0' + lastdigit);
 	return (lastdigit);
 }
