99d5eee7b053849c787ee5fdafe4c4be9727f6f4
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-21 22:13:12 +0300

    first attempt at question 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - short description
+ * @num: input character
+ * Return: 1 if pos, -1 if neg else 0
+ */
+int _abs(int num)
+{
+	if (num > 0)
+	{
+		return (num);
+	}
+	else if (num < 0)
+	{
+		return (-num);
+	}
+	else
+	{
+		return (0);
+	}
+}
