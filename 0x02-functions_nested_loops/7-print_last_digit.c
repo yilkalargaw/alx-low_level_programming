83f03924799d03dae8650d890b858a2c3856918e
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-22 00:14:26 +0300

    change the location of multiplication -1 because of minint in 7-print...

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,7 @@
 int print_last_digit(int num)
 {
 	unsigned int lastdigit;
-	(num >= 0) ? (lastdigit = num % 10) : (lastdigit = (-1 * num) % 10);
+	(num >= 0) ? (lastdigit = num % 10) : (lastdigit = (-1 * (num % 10)));
 	_putchar('0' + lastdigit);
 	return (lastdigit);
 }
