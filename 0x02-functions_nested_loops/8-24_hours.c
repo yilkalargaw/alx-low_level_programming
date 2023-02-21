1f8ab428d7b67a8f4e79a23d7291dc4d869a81e0
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-22 00:26:50 +0300

    tried solving the betty related issues

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
  * jack_bauer - print last digit
- * @num: input character
- * Return: last_digit of the
+ *
+ * Return: nothing
  */
 
 void jack_bauer(void)
 {
 	int i;
 
-	for(i = 0; i < 24; i++)
+	for (i = 0; i < 24; i++)
 	{
 		int j;
 
-		for(j = 0; j < 60; j++)
-			{
+		for (j = 0; j < 60; j++)
+		{
 				printf("%02d:%02d\n",i, j);
-			}
+		}
 	}
 }
