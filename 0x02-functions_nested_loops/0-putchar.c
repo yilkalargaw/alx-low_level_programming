98493d0b1e7751eacd523d9bb244305b7bb8223c
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-21 19:22:24 +0300

    First attempt at solution of 0x02 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - is the place we are printing a formatted string to stdout
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
