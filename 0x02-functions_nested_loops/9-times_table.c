f47b43ffd95470aa296f0a95a4f0cee15ed96814
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-22 00:48:21 +0300

    attempted 9-times-table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * times_table - print last digit
+ *
+ * Return: nothing
+ */
+
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 0; j < 10; j++)
+			printf("%2d%s", i*j, (j >= 9)? "\n" : ", ");
+	}
+}
