33e44df7b8e8a34872600e837dd3d3f92b0c6646
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-22 00:22:05 +0300

    attepmting 8-jack_bauer.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * jack_bauer - print last digit
+ * @num: input character
+ * Return: last_digit of the
+ */
+
+void jack_bauer(void)
+{
+	char i;
+
+	for(i = 0; i < 24; i++)
+	{
+		char j;
+
+		for(j=0; j<60; j++)
+			{
+				printf("%02d:%02d");
+			}
+	}
+}
