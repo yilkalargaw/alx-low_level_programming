bf5ffc078d028543db7353d120f3196b28a6c3c3
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-22 01:18:51 +0300

    attempt 11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @num: input number
+ * Return: Always 0.
+ */
+int print_to_98(int num)
+{
+    int i;
+
+	for (int i = num ; i <= 98; i++)
+		printf("%d%s", i, (i == 98) ? "\n" , ", ");
+
+	return (0);
+}
