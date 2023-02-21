efb3ccec48dc05ce0ba9f0c206697e837dd902fa
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-22 02:51:06 +0300

    attempted optional exercise 100-times_table...

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - print last digit
+ * @num: number to calculate n times table for
+ * Return: nothing
+ */
+
+void print_times_table(int num)
+{
+	int i;
+	if (0 <= num && num <= 15)
+		{
+			for (i = 0; i <= num; i++)
+				{
+					int j;
+
+					for (j = 0; j <= num; j++)
+						{
+							if (j == 0)
+								printf("0, ");
+							else
+								printf("%2d%s",
+									   i * j,
+									   (j >= num) ? "\n" : ", ");
+						}
+				}
+		}
+
+}
