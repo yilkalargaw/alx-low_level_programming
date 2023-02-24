e086ed5e9367be9197968eaf3bbb4fb9b0476052
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-25 02:30:11 +0300

    attepmting 10-print-triangle in 0x04

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints the alphabet using _putchar
+ * @size: input square size
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar(10);
+
+	for (i = size; i > 0; i--)
+	{
+		for (j = 1; j <= size; j++)
+			(j >= i) ? _putchar(35) : _putchar(32);
+		_putchar(10);
+	}
+}
