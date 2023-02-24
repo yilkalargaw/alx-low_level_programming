50ae2a5dcbdab83972f61c9fbeb75b527140a148
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-25 02:46:19 +0300

    trying to correct betty errors on 9-fizz... on 0x04...

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,10 @@ int main()
 	{
 		if ((i % 15) == 0)
 			printf ("FizzBuzz ");
-
 		else if ((i % 3) == 0)
 			printf("Fizz ");
-
 		else if ((i % 5) == 0)
 			(i == 100) ? printf("Buzz\n") : printf("Buzz ");
-
 		else
 			printf("%d ", i);
 	}
