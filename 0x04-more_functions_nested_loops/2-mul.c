45c27bf98afa6faacb85896f09ad3f976c9a7014
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-24 22:42:57 +0300

    attempted 3-mul.c in 0x04

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ *
+ * Return: multiplication result
+ */
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,5 +3,6 @@
 
 int _isupper(int);
 int _isdigit(int);
+int mul(int a, int b);
 
 #endif
