7711efbacffdeef77f05d0a0c4ff3c4affe45a50
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-24 23:23:39 +0300

    attempted 6-print_line on 0x04

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - prints the alphabet using _putchar
+ *
+ * Return: void
+ */
+
+void print_line(int n);
+{
+	for (i = 0; i < n ; i++ )
+		{
+			_putchar('_');
+		}
+	_putchar(10);
+}
