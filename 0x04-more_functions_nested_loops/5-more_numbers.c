58ded84486ca568a61ccced0b8b991a8c57cb9ca
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-24 23:11:10 +0300

    iterated on 5-more_numbers.c on 0x04

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@
 
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0 ; i < 10; i++)
+	for (j = 0 ; j < 10; j++)
 	{
 		for (i = 0 ; i < 15; i++)
 		{
@@ -17,6 +17,6 @@ void more_numbers(void)
 				_putchar('0' + (i / 10));
 			_putchar('0' + (i % 10));
 		}
+		_putchar(10);
 	}
-	_putchar(10);
 }
