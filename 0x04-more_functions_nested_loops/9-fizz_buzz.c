e5e20b07588cd0ef6dd5cd7a348d2c7fca8832a9
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-25 02:44:20 +0300

    attempting 9-fizz_buzz in 0x04...

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet using _putchar
+ *
+ * Return: void
+ */
+int main()
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 15) == 0)
+			printf ("FizzBuzz ");
+
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+
+		else if ((i % 5) == 0)
+			(i == 100) ? printf("Buzz\n") : printf("Buzz ");
+
+		else
+			printf("%d ", i);
+	}
+	return 0;
+}
