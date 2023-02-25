8763a74970e70c20d7b92c48ee374f83a2c1e50d
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-25 03:44:29 +0300

    attempting 101-print_number on 0x04

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ * Return: void
+ */
+void print_number(int n)
+{
+	int magnitude = n;
+
+	/* extract magnitude */
+	if (n < 0)
+		magnitude = -1 * n;
+
+	if (n < 0)
+		_putchar('-');
+
+	while (magnitude / 10 != 0 )
+		{
+			_putchar('0' + (magnitude % 10));
+			magnituder /= 10;
+		}
+
+}
