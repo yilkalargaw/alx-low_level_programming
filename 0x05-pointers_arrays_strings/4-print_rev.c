8223639a0470fbc9387d466e25aca7e6f183cfed
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-27 22:21:45 +0300

    iterated on 4-print_rev @ 0x05...

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,8 +9,8 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = -1; *(s + i) != 0; i++);
-	for (i = i ; i >= 0; i--)
+	for (i = 0; *(s + i) != 0; i++);
+	for (i = i - 1; i >= 0; i--)
 		_putchar(*(s + i));
 	_putchar(10);
 }
