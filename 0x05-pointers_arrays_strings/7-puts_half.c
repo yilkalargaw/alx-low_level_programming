d92d7d4a1674ababd5d6a49cba2efeaa4d4194c6
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-27 23:08:43 +0300

    attempting 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: pointer to string
+ *
+ * Return: len (Size s).
+ */
+void _puts(char *str)
+{
+	int i = 0;
+	int len;
+
+	len = 0;
+	while (s[len] != 0)
+		len++;
+
+	for (i = 0; i <= len / 2 ; i++)
+		_putchar(*(str + i));
+
+	_putchar(10);
+}
