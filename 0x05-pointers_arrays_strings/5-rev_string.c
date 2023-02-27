6ddeb8b45b72d063d6d966bd56252c4b307d8aba
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-27 22:52:05 +0300

    attempting 5-re_string on 0x05

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s: input pointer to string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+	char tmp[100];
+
+	for (i = 0; *(s + i) != 0; i++)
+		tmp[i] = *(s + i);
+
+	for (i--, j = 0; i >= 0; i--, j++)
+		*(s + j) = tmp[i];
+}
