834af47f15aa93e0304adc61b98abe5f97718c0c
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-28 02:17:31 +0300

    reverted 5-rev_string on 0x05 and renamed the file missing

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,9 +12,9 @@ void rev_string(char *s)
 
 	char tmp[1000];
 
-	for (i = 0; s[i] != 0; i++)
-		tmp[i] = s[i];
+	for (i = 0; *(s + i) != 0; i++)
+		tmp[i] = *(s + i);
 
 	for (i -= 1, j = 0; i >= 0; i--, j++)
-		s[j] = tmp[i];
+		*(s + j) = tmp[i];
 }
