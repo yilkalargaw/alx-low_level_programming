42c08e48a4ace88fa6ac1e69673591913b46dcc8
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-27 20:21:18 +0300

    attempting 3-puts.c form 0x05

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - prints a string
+ * @str: pointer to string
+ *
+ * Return: len (Size s).
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	for( i = 0; *(str + i) != 0 ; i++)
+		_putchar(*(str + i));
+
+	_putchar(10);
+}
