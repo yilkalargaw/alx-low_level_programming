39b4c172ef168809350f11363653b9c03f04967d
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-27 22:13:42 +0300

    attempt 4-print_rev in 0x05

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - print a string reverse
+ * @s: pointer to char input
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = -1;
+
+	for (i = 0; *(s + i) != 0; i++);
+	for (i; i >= 0; i--)
+		_putchar(*(s + i));
+	_putchar(10);
+}
