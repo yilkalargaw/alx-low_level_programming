e88c2ca4fd8185db8c00b9fd4165d08f497f6d61
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-27 22:59:15 +0300

    attempting 6-puts2 in 0x05...

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints every other char in a string
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	for (i = 0; *(str + i) != 0 ; i++)
+	{
+		if(i % 2 == 0)
+			_putchar(*(str + i));
+	}
+
+	_putchar(10);
+}
