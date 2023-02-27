c0feea770a41e10fef6a91aa0067da1e223211ad
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-27 23:52:52 +0300

    iterate on 8-print_array in 0x05...

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,6 +12,6 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 		printf("%s,%s",
-			   (n >= 0) ? (sprintf("%i", *(a + i))) : "\n",
+			   (n >= 0) ? sprintf("%i", *(a + i)) : "\n",
 			   (i >= n - 1) ? "\n" : ", ");
 }
