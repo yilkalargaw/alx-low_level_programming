90ed366fe41bc76bd24d665a1ff6594ee98accb3
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-27 20:13:41 +0300

    attempting 2-strlen in 0x05....

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strlen - return length of a string
+ *@s: pointer to string
+ *
+ *Return: length
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != 0)
+		len++;
+
+	return (len);
+}
