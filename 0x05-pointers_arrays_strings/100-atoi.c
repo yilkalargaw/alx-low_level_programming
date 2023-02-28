b90fbf3f2005325ca0ebc480b17c203f925b3435
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-28 03:14:03 +0300

    attempting 100-atoi.c for 0x05...

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _atoi - convert to numbers array
+ * @s string pointer
+ *
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int num;
+	int i;
+	int len;
+
+	num=0;
+
+	len = 0;
+	while (s[len] != 0)
+		len++;
+
+
+	if (s[0] == '-')
+	{
+		for(i = 1; i<len; i++)
+		{
+			if (s[i] >= 48 && s[i] <= 57)
+				num = (num * 10) + (s[i] - 48);
+			else
+				continue;
+		}
+		return (-1 * num);
+	}
+	else
+	{
+		for(i = 0; i<len; i++)
+		{
+			if (s[i] >= 48 && s[i] <= 57)
+				num = (num * 10) + (s[i] - 48);
+			else
+				continue;
+		}
+		return (num);
+	}
+}
