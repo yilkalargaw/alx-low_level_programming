993c2aed720708f49c138c192f5a3daaa8ff8a66
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-02-28 03:30:50 +0300

    attempting 100-atoi.c at 0x05...

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,34 +10,25 @@ int _atoi(char *s)
 	int num;
 	int i;
 	int len;
+	int sign;
 
 	num = 0;
+	sign = 1;
 
 	len = 0;
 	while (s[len] != 0)
 		len++;
 
 
-	if (s[0] == '-')
+	for (i = 0; i < len; i++)
 	{
-		for (i = 1; i < len; i++)
-		{
-			if (s[i] >= 48 && s[i] <= 57)
-				num = (num * 10) + (s[i] - 48);
-			else
-				continue;
-		}
-		return (-1 * num);
+		if (s[i] >= 48 && s[i] <= 57)
+			num = (num * 10) + (s[i] - 48);
+		else(s[i] == '-')
+				sign *= -1;
+		else
+			continue;
 	}
-	else
-	{
-		for (i = 0; i < len; i++)
-		{
-			if (s[i] >= 48 && s[i] <= 57)
-				num = (num * 10) + (s[i] - 48);
-			else
-				continue;
-		}
-		return (num);
-	}
+		return (num * sign);
+
 }
