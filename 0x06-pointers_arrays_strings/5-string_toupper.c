1a3ef12ae932ffb512a9e0908ba79a5ec4490b90
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-02 23:32:52 +0300

    dumb commit on 5-sting... on 0x06..

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,10 +11,12 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != 0; i++)
 	{
 		if (s[i] >= 97 && s[i] <= 122)
 			s[i] = s[i] - 32;
+		else
+			continue;
 	}
 
 	return (s);
