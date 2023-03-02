38d072019425b39233222519bb84e67284bc3767
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-02 22:12:18 +0300

    attempting 4-reverse_array on 0x06...

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - compares two strings
+ * @a: pointer to array of strings
+ * @n: length
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i,
+	int temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - i - 1] = temp;
+	}
+}
