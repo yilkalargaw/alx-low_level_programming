85598099cd136cc3d625b5dc7caf5f972ede30af
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-02 23:18:44 +0300

    forgot about the if thing so iterate on 7-leet.c on 0x06...

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,25 +6,21 @@
  *
  * Return: pointer to output string
  */
+
 char *leet(char *s)
 {
+	char *text = "aAeEoOtTlL";
+	char *replacements = "4433007711";
 	int i = 0;
+	int j = 0;
 
-	while (s[i])
+	for (; s[i] != 0; i++)
 	{
-		if (s[i] == 'A' || s[i] == 'a')
-			s[i] = '4';
-		else if (s[i] == 'E' || s[i] == 'e')
-			s[i] = '3';
-		else if (s[i] == 'O' || s[i] == 'o')
-			s[i] = '0';
-		else if (s[i] == 'T' || s[i] == 't')
-			s[i] = '7';
-		else if (s[i] == 'L' || s[i] == 'l')
-			s[i] = '1';
-
-		i++;
+		for (j = 0; j < 10 ; j++)
+		{
+			if (s[i] == text[j])
+				s[i] = replacements[j];
+		}
 	}
-
-	return (s);
+	return s;
 }
