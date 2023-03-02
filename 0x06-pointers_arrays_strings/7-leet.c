259e5a6dbd0da23554f6224ac34c219391f718fd
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-02 22:43:02 +0300

    attempting 7-leet.c on 0x06...

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include <main.h>
+
+/**
+ * leet - encodes a string to 1337
+ * @s: pointer to input string
+ *
+ * Return: pointer to output string
+ */
+char *leet(char *s)
+{
+    int i = 0;
+
+    while (s[i])
+    {
+        if (s[i] == 'A' || s[i] == 'a')
+            s[i] = '4';
+        else if (s[i] == 'E' || s[i] == 'e')
+            s[i] = '3';
+        else if (s[i] == 'O' || s[i] == 'o')
+            s[i] = '0';
+        else if (s[i] == 'T' || s[i] == 't')
+            s[i] = '7';
+        else if (s[i] == 'L' || s[i] == 'l')
+            s[i] = '1';
+
+        i++;
+    }
+
+    return (s);
+}
