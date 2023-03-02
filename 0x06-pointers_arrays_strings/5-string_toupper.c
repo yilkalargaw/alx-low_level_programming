1dca2edd139a8cb969497c4f59c28f6ba2258eaa
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-02 22:21:59 +0300

    attempting 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - compares two strings
+ * @s: pointer to string s
+ *
+ * Return: char * of reversed string
+ */
+
+char *string_toupper(char *s)
+{
+  int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+	}
+
+	return s;
+}
