7ff601b67affa2fbaee1d431fdc436aa8742209b
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-02 21:55:09 +0300

    attempting 3-strcmp on 0x06...

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncmp - compares two strings
+ * @s1: input pointer char*
+ * @s2: output pointer char*
+ *
+ * Return: pointer to destination
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	char *str_one = s1;
+	char *str_two = s2;
+
+	for (;*str_one != 0 && *str_two != 0 && *str_one == *str_two;
+		str_one++, str_two++)
+		;
+
+	return (*str_one - *str_two);
+}
