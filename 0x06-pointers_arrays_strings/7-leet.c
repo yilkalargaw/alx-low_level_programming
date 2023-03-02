091122cb3b3b609484449a937159482f1d37cb9d
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-02 22:47:55 +0300

    tabifying 7-leet.c 0x06...

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,23 +8,23 @@
  */
 char *leet(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-    {
-        if (s[i] == 'A' || s[i] == 'a')
-            s[i] = '4';
-        else if (s[i] == 'E' || s[i] == 'e')
-            s[i] = '3';
-        else if (s[i] == 'O' || s[i] == 'o')
-            s[i] = '0';
-        else if (s[i] == 'T' || s[i] == 't')
-            s[i] = '7';
-        else if (s[i] == 'L' || s[i] == 'l')
-            s[i] = '1';
+	while (s[i])
+	{
+		if (s[i] == 'A' || s[i] == 'a')
+			s[i] = '4';
+		else if (s[i] == 'E' || s[i] == 'e')
+			s[i] = '3';
+		else if (s[i] == 'O' || s[i] == 'o')
+			s[i] = '0';
+		else if (s[i] == 'T' || s[i] == 't')
+			s[i] = '7';
+		else if (s[i] == 'L' || s[i] == 'l')
+			s[i] = '1';
 
-        i++;
-    }
+		i++;
+	}
 
-    return (s);
+	return (s);
 }
