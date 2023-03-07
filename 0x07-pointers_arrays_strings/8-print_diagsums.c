6a773d76d54e81f1e7c9e1cdf5093795f15c978d
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-07 03:31:45 +0300

    iterating becoause I forgot to tabify on 8-print_diag... on 0x07...

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,16 +10,16 @@
 
 void print_diagsums(int *a, int size)
 {
-    int i= 0, sum1 = 0, sum2 = 0;
-    for (; i < size * size; i++)
-    {
-        if (i % (size + 1) == 0)
-            sum1 += a[i];
+	int i= 0, sum1 = 0, sum2 = 0;
+	for (; i < size * size; i++)
+	{
+		if (i % (size + 1) == 0)
+			sum1 += a[i];
 
-        if (i % (size - 1) == 0 &&
+		if (i % (size - 1) == 0 &&
 			i != 0 && i != size * size - 1)
-            sum2 += a[i];
-    }
+			sum2 += a[i];
+	}
 
-    printf("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
