35e565e4d197257ca70bcad31fc11deb6d3925c0
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-07 03:02:07 +0300

    return to nested loops instead of bitmasking on 5-strstr on 0x07...
    based on advice from slack and because i was running out of time

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,34 +10,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned long mask[256] = {0};
-	unsigned long needle_len = 0;
-	char *p;
+	unsigned int i=0, flag = 0;
+	unsigned int j;
 
-	/* Set the bits corresponding to the characters in the needle */
-	for (p = needle; *p; p++)
+	if (*needle == 0)
+		return (haystack);
+
+	for (; *(haystack + i) != 0; ++i)
 	{
-		mask[(unsigned char)*p] |= 1UL << needle_len;
-		needle_len++;
+		for (j = 0; *(needle + j) != 0; ++j)
+			if (*(needle + j) == *(haystack + i + j))
+				flag++;
+
+		if (*(needle + flag) == 0)
+			return (haystack + i);
+
+		flag = 0;
 	}
 
-	/* Search for the needle in the haystack */
-	for (p = haystack; *p; p++)
-	{
-		unsigned long state = 0;
-		char *q;
-
-		/* Check if the current character matches any character in the needle */
-		for (q = p; *q && (state |= mask[(unsigned char)*q]) < (1UL << needle_len); q++)
-		{
-			state <<= 1;
-		}
-
-		/* Check if the needle is found */
-		if (state == (1UL << needle_len))
-			return (p);
-	}
-
-	/* Return '\0' if no match is found */
 	return (0);
 }
