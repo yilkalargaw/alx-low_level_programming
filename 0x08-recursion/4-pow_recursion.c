c2b53e3069e60ea181451f2c186b4c237aaaa5e9
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-08 23:19:54 +0300

    faild build for some reason on 4-pow... on 0x08...

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,8 +13,8 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	else (y == 1)
+	else if (y == 1)
 		return (x);
-
-	return (x * _pow_recursion(x, y - 1));
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
