1a55ece034e8e29ab9a67d1b5ee6040621614294
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-09 01:04:52 +0300

    tabify to solve betty related issues on 100_is... on 0x08...

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,10 +8,10 @@
  */
 int str_len(char *str)
 {
-    if (*str == '\0')
-        return 0;
+	if (*str == '\0')
+		return 0;
 
-    return 1 + str_len(str + 1);
+	return 1 + str_len(str + 1);
 }
 
 /**
