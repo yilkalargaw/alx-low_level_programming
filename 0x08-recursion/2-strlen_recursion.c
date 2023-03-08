5d5df70f50a857e343d51497839970459b92a995
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-08 20:48:19 +0300

    attempting 2-strlen_recursion 0x08

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - ex 2 on 0x08
+ * @s: string pointer
+ *
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
+}
