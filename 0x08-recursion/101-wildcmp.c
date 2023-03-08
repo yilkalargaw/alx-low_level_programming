acde0cd30c6388870054bf79de0e380d516c1b99
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-09 01:25:26 +0300

    attempting 101-wildcmp on 0x08...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+/**
+ * wildcmp - calculates factorial
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: wildcmp? as int
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == 0 && *s2 == 0)
+		return (1);
+
+	if (*s2 == 0)
+		return (0);
+
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != 0 && wildcmp(s1 + 1, s2)));
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
