26f7f8124926ca4f36a2cf958bc6decadd15468b
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-08 20:33:16 +0300

    attempting 0-put_recursion on 0x08...

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - ex 0 on 0x08
+ * @s: string pointer
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (!s || !*s)
+		_putchar(10);
+	else
+		_putchar(*s);
+		_puts_recursion(s + 1);
+}
