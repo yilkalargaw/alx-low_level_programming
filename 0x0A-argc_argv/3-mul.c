e611bb43867761f8c9a307dca8766148a7d21b6f
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-10 23:23:46 +0300

    attempting 3-mul.c on 0x0A...

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers.
+ * @argc: input size
+ * @argv: input string array
+ *
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%i\n", num1 * num2);
+
+	return (num1 * num2);
+}
