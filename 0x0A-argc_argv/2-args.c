b3488e2273e4739c67a9982277049a7cb67de2f8
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-10 23:09:21 +0300

    attempting 2-args.c on 0x0A...

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints argument number
+ * @argc: input size
+ * @argv: input string array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i=1;
+
+	for(; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
