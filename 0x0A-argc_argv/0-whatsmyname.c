175c6493d55b864b77c20f4872ace7072f1eead4
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-10 22:50:08 +0300

    attempting 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* #include <stdlib.h> */
+/**
+ * main - prints prog name
+ * @argc: input size
+ * @argv: input string array
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
