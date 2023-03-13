19b549d57fb4af941eb789fecca7b3386e8012ea
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-13 10:41:28 +0300

    attempting 100-change.c on 0x0A...

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - calculates coins
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int cents;
+	int coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (; cents > 0; coins++)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -=2;
+		else
+			cents -=1;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
+}
