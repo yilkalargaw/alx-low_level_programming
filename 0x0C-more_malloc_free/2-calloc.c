bce441ccb2a6e555daa10b50f1dcdad28060e05b
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-16 21:08:22 +0300

    attempting 2-calloc.c on 0x0C...

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate memory using malloc
+ * @nmemb: number of elements
+ * @size: size
+ *
+ * Return: pointer to the allocated memory, or NULL if nmemb or size is 0 or if
+ * malloc fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
