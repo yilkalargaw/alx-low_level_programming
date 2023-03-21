4e7671a392d3c9392cd6305d3045f1836463fa8d
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-21 03:42:51 +0300

    attempting 2-print_dog on 0x0E...

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints input dog-struct
+ *
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\nAge: %f\nOwner: %s\n",
+		   d -> name, d -> age, d -> owner);
+}
