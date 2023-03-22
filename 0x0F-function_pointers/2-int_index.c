6d6fc45f41ecd301f3dfa851d6a24e81ada7d3b3
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-23 02:58:59 +0300

    solving operator space issue on betty in 2-int_index on 0xOF...

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (cmp != NULL && array !=NULL)
+	if (cmp != NULL && array != NULL)
 		for (; i < size; i++)
 			if (cmp(array[i]))
 				return (i);
