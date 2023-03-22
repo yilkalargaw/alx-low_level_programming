d3be997f55105cf768e81c2bb66a946a2b20cfe9
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-03-23 02:49:24 +0300

    solving betty space issue on 1-array_iterator in 0x0F...

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL && action !=NULL)
+	if (array != NULL && action != NULL)
 	{
 		while (size-- > 0)
 			action(*array++);
