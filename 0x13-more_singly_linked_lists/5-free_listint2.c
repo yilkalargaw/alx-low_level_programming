4382af967b7302421635919841c2ab4176f31492
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-04-03 19:37:44 +0000

    attempting 5-free_listint2.c on 0x13...

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "list.h"
+
+/**
+ * free_listint2 - Frees frees a listint_t list
+ * @head: Pointer to the head of the list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	for (; *head != NULL; *head = (*head)->next, free(current))
+		current = *head;
+
+	*head = NULL;
+}
