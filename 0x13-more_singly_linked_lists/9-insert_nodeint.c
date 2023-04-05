6a87304d72b68d64d4d0f0f06fc66c67b46c6d10
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-04-05 02:18:47 +0000

    change 9 to recursive mode because someone suggested on slack on 0x13...

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,28 +9,22 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current_node = NULL, *new_node = NULL;
-	unsigned int i = 0;
-
-	 if (head == NULL)
+	if (!head)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)  /* check if malloc worked */
-		return (NULL);
+	if (idx && *head)
+		return (insert_nodeint_at_index(&(*head)->next, idx - 1, n));
 
-	new_node->n = n;
+	if (idx == 0)
+	{
+		listint_t *new = malloc(sizeof(listint_t));
+		if (!new)
+			return (NULL);
 
-	idx == 0 ? (new_node->next = *head, *head = new_node) : 0;
-
-	current_node = *head;
-	for (; current_node != NULL && i < idx - 1; i++)
-		current_node = current_node->next;
-
-	current_node == NULL ? (free(new_node), new_node = NULL) : 0;
-
-	current_node != NULL ? (new_node->next = current_node->next,
-							current_node->next = new_node) : 0;
-
-	return (new_node);
+		new->n = n;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	return (NULL);
 }
