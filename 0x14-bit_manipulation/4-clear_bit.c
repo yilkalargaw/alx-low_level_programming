5c9bdd8c8113139a173344420db60be6038d34cd
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-04-06 19:59:19 +0000

    improve doc-comment on 4-clear_bit.c on 0x14...

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * clear_bit - sets the value of a bit to 1 at a given index
+ * clear_bit - sets the value of a bit to 0 at a given index
  * @n: input int
  * @index: input index
  *
