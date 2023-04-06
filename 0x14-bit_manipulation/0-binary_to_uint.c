417150e818aea1ce27a8fde537c881208c67c906
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-04-06 18:36:04 +0000

    iterating on 0-binary_to_uint.c on 0x14...

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,7 +2,7 @@
 
 
 /**
- * binary_to_uint -  converts a binary number to an unsigned int
+ * binary_to_uint -	 converts a binary number to an unsigned int
  * @b: is a pointer to a string of 0 and 1 chars
  *
  * Return: unsigned int with the decimal value of the num or zero
@@ -10,6 +10,9 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
+
+	if (b == NULL)
+		return (0);
 
 	for (; *b != 0 ; b++)
 	{
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define NULL ((void *)0)
+
 unsigned int binary_to_uint(const char *b);
 
 #endif /* MAIN_H */
