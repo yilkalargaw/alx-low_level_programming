96da2a54cac5ddccedea53da58ea849eb5c9ea93
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-04-10 19:59:51 +0000

    attempting 0-read_textfile.c in 0x15...

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * read_textfile - prints text file to the POSIX stdout
+ * @filename: input file name
+ * @letters: input file name
+ *
+ * Return: number of characters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t r, w; /* read and write sizes */
+	int fd; /* file discriptor most examples use this variable */
+	char *buffer; /* string buffer */
+
+	if (!filename ||
+		(fd = open(filename, O_RDONLY)) == -1 ||
+		!(buffer = malloc(sizeof(char) * letters)))
+		return (0);
+
+	r = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	close(fd);
+	free(buffer);
+
+	return (w);
+}
