7d398124e90a95a2774188e9a8f6fc7a922b6f48
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-04-10 21:54:25 +0000

    created a template for 3-cp.c on 0x15...

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,11 @@
+/**
+ * main - copies contents of a file
+ * @argc: Number of arguments passed
+ * @argv: Array of pointers to arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+    return (0);
+}
