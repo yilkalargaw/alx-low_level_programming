211c3348067db26f2c136376264ecadea3099808
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-04-10 20:40:22 +0000

    attempting 1-create_file.c on 0x15...

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ *_strlen - return length of a string
+ *@s: pointer to string
+ *
+ *Return: length
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != 0)
+		len++;
+
+	return (len);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: input file name string
+ * @text_content: input string
+ *
+ * Return: integer showing status
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t w; /* r,  /\* read and write sizes *\/ */
+	int fd; /* file discriptor most examples use this variable */
+
+	bool check = (!filename ||
+				  (fd = open(filename,
+							 O_CREAT |
+							 O_RDWR |
+							 O_TRUNC, 0600)) == -1) ? false : true;
+	if (check != true)
+		return (-1);
+
+	text_content = (text_content) ? text_content : "";
+
+	w = write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+
+
+	return ((w == -1) ? -1 : w);
+}
