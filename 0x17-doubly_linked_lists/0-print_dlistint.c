ab6fe16054b41904e29b3efa1012701d85b2bebe
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-19 01:07:27 +0000

    attempting 0-print_dlistint.c on 0x17...

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of a doubly-linked list
+ * @h: ptr to head of dobuly-linked list
+ *
+ * Return: num of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+		return (i);
+
+	for (;h->next != NULL; h = h->next,i++)
+		printf("%d\n", h->n);
+
+	printf("%d\n", h->n);
+
+	return (i + 1);
+}
