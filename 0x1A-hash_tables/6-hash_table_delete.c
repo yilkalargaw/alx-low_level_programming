9bb29b83f7aca6ea47aa794fbbea9a69ae9574bc
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-07-08 05:14:24 +0300

    iterating on 6-hash_table_delete on 0x1A-hash_tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,18 +1,18 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - fonction for deleting a hash table.
+ * hash_table_delete - function for deleting a hash table.
  * @ht: pointer to hash table to be deleted.
  *
  * Return: Nothing.
  */
 void hash_table_delete(hash_table_t *ht) {
-	hash_node_t *current_node, *next_node;
+	hash_node_t *current_node;
+	hash_node_t *next_node;
 	unsigned long int i;
 
 	if (ht == NULL)
 		return;
-
 
 	for (i = 0; i < ht->size; i++)
 		{
