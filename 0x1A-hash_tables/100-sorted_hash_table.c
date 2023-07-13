28edf3bf7acf427ca08131902016c3272c2a4abf
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-07-13 05:53:29 +0300

    iterating on 0x1A-hash_tables/100-sorted_hash_table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -31,27 +31,27 @@ shash_table_t *shash_table_create(unsigned long int size)
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
-int shash_table_set(shash_table_t *ht, const char *key, const char *value)
-{
-	unsigned long int tbl = hash_function(key);
-	shash_node_t *node = ht->array[tbl];
+/* int shash_table_set(shash_table_t *ht, const char *key, const char *value) */
+/* { */
+/* 	unsigned long int tbl = hash_function(key); */
+/* 	shash_node_t *node = ht->array[tbl]; */
 
-	for (; node != NULL; node = node->next)
-	{
-		if (strcmp(node->key, key) == 0)
-		{
-			node->value = value;
-			return (1);
-		}
-	}
+/* 	for (; node != NULL; node = node->next) */
+/* 	{ */
+/* 		if (strcmp(node->key, key) == 0) */
+/* 		{ */
+/* 			node->value = value; */
+/* 			return (1); */
+/* 		} */
+/* 	} */
 
-	node = malloc(sizeof(shash_node_t));
-	node->key = key;
-	node->value = value;
-	node->next = ht->array[tbl];
-	ht->array[tbl] = node;
-	return (0);
-}
+/* 	node = malloc(sizeof(shash_node_t)); */
+/* 	node->key = key; */
+/* 	node->value = value; */
+/* 	node->next = ht->array[tbl]; */
+/* 	ht->array[tbl] = node; */
+/* 	return (0); */
+/* } */
 
 /**
  * shash_table_get - gets value associated with a hash-table key
