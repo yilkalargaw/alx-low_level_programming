23ecce49339e5b6b3e3f4e4d1366a211bf73ac2f
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-07-13 05:35:52 +0300

    iterationg on 0x1A-hash_tables/100-sorted_hash_table to enable compilation

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -61,7 +61,14 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
  */
 char *shash_table_get(const shash_table_t *ht, const char *key)
 {
-	return('A');
+	unsigned long int tbl = hash_function(key);
+	shash_node_t *node = ht->array[tbl];
+	char* tmp= "hello";
+
+	tbl++;
+	node++;
+
+	return(tmp);
 }
 
 /**
@@ -72,7 +79,11 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
  */
 void shash_table_print(const shash_table_t *ht)
 {
+	unsigned long int tbl = hash_function(key);
+	shash_node_t *node = ht->array[tbl];
 
+	tbl++;
+	node++;
 }
 
 /**
@@ -83,6 +94,11 @@ void shash_table_print(const shash_table_t *ht)
  */
 void shash_table_print_rev(const shash_table_t *ht)
 {
+	unsigned long int tbl = hash_function(key);
+	shash_node_t *node = ht->array[tbl];
+
+	tbl++;
+	node++;
 
 }
 
@@ -94,5 +110,10 @@ void shash_table_print_rev(const shash_table_t *ht)
  */
 void shash_table_delete(shash_table_t *ht)
 {
+	unsigned long int tbl = hash_function(key);
+	shash_node_t *node = ht->array[tbl];
+
+	tbl++;
+	node++;
 
 }
