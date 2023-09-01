ceed31507d42f9d7264177cf99c95a2342e38f1f
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-01 02:09:04 +0000

    iterating on 0x1C-makefiles/5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,12 +5,20 @@ Module for find the perimeter
 """
 
 
+#!/usr/bin/python3
 def island_perimeter(grid):
     """Method for calculate the perimeter of the island"""
 
-    return sum(4 - (grid[line][col - 1] +
-                    grid[line][col + 1] +
-                    grid[line - 1][col] +
-                    grid[line + 1][col])
-               for line in range(len(grid))
-               for col in range(len(grid[line])) if grid[line][col] == 1)
+    # Create a new list that contains only valid elements
+    valid_grid = [[0] * (len(grid[0]) + 2)]
+    valid_grid += [[0] + row + [0] for row in grid]
+    valid_grid.append([0] * (len(grid[0]) + 2))
+
+    # Calculate the perimeter of the island
+    perimeter = sum(4 - (valid_grid[i][j - 1] + valid_grid[i][j + 1] +
+                         valid_grid[i - 1][j] + valid_grid[i + 1][j])
+                     for i in range(1, len(valid_grid) - 1)
+                     for j in range(1, len(valid_grid[i]) - 1)
+                     if valid_grid[i][j] == 1)
+
+    return perimeter
